e0b8d160b97d8947289ab31135aeb9c901831eea
honk <honklol@protonmail.com>
2022-08-10 15:30:56 +0200

    Update bashrc.sh

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script as root."
+  exit
+fi
+
 apt install patch curl -y
-curl -L https://github.com/honklol/patches/raw/main/bashrc.patch | patch -u /root/.bashrc
+curl -L https://github.com/honklol/patches/raw/main/patches/bashrc.patch | patch -u /root/.bashrc
