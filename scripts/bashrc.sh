4f25f12f2cf909ff4f872c5f079d804b090fff95
honk <honklol@protonmail.com>
2022-08-10 15:44:04 +0200

    Update bashrc.sh

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -5,5 +5,13 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+function cline { tput cuu1;tput el; }
+exec 3>&1 &>/dev/null
+
+echo ""; echo "Running apt update" >&3
+apt update
+cline; echo "Installing patch and curl" >&3
 apt install patch curl -y
+cline; echo "Patching .bashrc" >&3
 curl -L https://github.com/honklol/patches/raw/main/patches/bashrc.patch | patch -u /root/.bashrc
+cline; echo "Patched .bashrc - reset your session to apply changes." >&3
